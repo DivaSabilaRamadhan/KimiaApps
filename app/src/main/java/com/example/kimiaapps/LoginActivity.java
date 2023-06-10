9e673a861fc5fe10f9e30b07827f18fd62b90cc6
Diva <divasr9i@gmail.com>
2023-06-10 10:12:06 +0700

    Login aktiviti

--- app/src/main/java/com/example/kimiaapps/LoginActivity.java
+++ app/src/main/java/com/example/kimiaapps/LoginActivity.java
@@ -1,14 +1,64 @@
 package com.example.kimiaapps;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+    FirebaseAuth firebaseAuth;
+    EditText email, password;
+    Button masuk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        masuk = (Button) findViewById(R.id.b_signin);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        masuk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mail = email.getText().toString();
+                String pass = password.getText().toString();
+
+                if (TextUtils.isEmpty(mail)) {
+                    email.setError("Email tidak boleh kosong");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(pass)) {
+                    password.setError("Password tidak boleh kosong");
+                    return;
+                }
+
+                firebaseAuth.signInWithEmailAndPassword(mail, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Username atau Password salah", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 }
